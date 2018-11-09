08e44956e1502be622b8ef6dd6220095cfb95fda
Olavi Sau <olavisau@gmail.com>
2018-11-09 14:52:29 +0200

    Clean up categories and setup achievements

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {AppState} from '../app.state';
+import {Store} from '@ngrx/store';
+import {Category} from '../models/category';
 
 @Component({
   selector: 'ad-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
-export class CategoryComponent implements OnInit {
+export class CategoryComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  category: Category;
+  private routeSubscription: any;
+
+  constructor(private store: Store<AppState>, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.routeSubscription = this.route.params.subscribe((params: { key: string }) =>
+      this.store.select('categories').subscribe(
+        categories => this.category = categories.find(category => category.key === params.key)
+      )
+    );
   }
 
+  ngOnDestroy() {
+    this.routeSubscription.unsubscribe();
+  }
 }
