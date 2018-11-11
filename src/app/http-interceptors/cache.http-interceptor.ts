c8a8a2646267ceb0858f21cc30214941c9324b62
Olavi Sau <olavisau@gmail.com>
2018-11-11 16:01:04 +0200

    Make cache a bit less naive

--- src/app/http-interceptors/cache.http-interceptor.ts
+++ src/app/http-interceptors/cache.http-interceptor.ts
@@ -10,6 +10,8 @@ export class CachingInterceptor implements HttpInterceptor {
   constructor(private cache: AppRequestCache) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
+    if (req.method !== 'GET') { return next.handle(req); }
+
     return this.cache.get(req) ? of(this.cache.get(req)) : next.handle(req).pipe(
       tap(event => {
         if (event instanceof HttpResponse) {
