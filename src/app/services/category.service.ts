a8c205b7f4393b634d2867f0bc580c322ec36fe6
Olavi Sau <olavisau@gmail.com>
2018-11-12 18:44:31 +0200

    Improve ordering

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -8,17 +8,10 @@ import {map} from 'rxjs/operators';
 })
 export class CategoryService {
 
-  constructor(private http: HttpClient) { }
-
-  save(category: CategoryModel) {
-    return this.http.put(`http://api.achievement.ee:8080/category/${category.key}`, category).subscribe(
-      response => console.log(response)
-    );
-  }
+  constructor(private http: HttpClient) {}
 
   get(): Promise<CategoryModel[]>;
   get(key: string): Promise<CategoryModel>;
-
   get(key?: string) {
     if (!key) {
       return this.http.get<CategoryModel[]>('http://api.achievement.ee:8080/category').pipe(
@@ -28,6 +21,12 @@ export class CategoryService {
     return this.http.get<CategoryModel>(`http://api.achievement.ee:8080/category/${key}`).pipe(
       map( category => new CategoryModel(category))
     ).toPromise();
+  }
+
+  save(category: CategoryModel) {
+    return this.http.put(`http://api.achievement.ee:8080/category/${category.key}`, category).subscribe(
+      response => console.log(response)
+    );
   }
 
 }
