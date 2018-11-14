b5a4ddc164281afe1afd2cf9407c4e3b3975966f
Olavi Sau <olavisau@gmail.com>
2018-11-14 04:58:21 +0200

    Use obvservable once more

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -4,32 +4,29 @@ import {Store} from '@ngrx/store';
 import {AppState} from '../../app.state';
 import {SaveCategoryAction} from '../../actions/save-category.action';
 import {CategoryCollection} from '../../collections/category.collection';
-import {Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {CategoryService} from '../../services/category.service';
 import {SetCategoriesAction} from '../../actions/set-categories.action';
 import {SubscriptionCollector} from '../../subscription-collector';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'ad-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss']
 })
-export class SummaryComponent implements OnInit, OnDestroy, SubscriptionCollector {
+export class SummaryComponent implements OnInit{
 
   constructor(private store: Store<AppState>) {}
 
-  collection: CategoryCollection;
+  collection: Observable<CategoryCollection>;
   category: CategoryModel;
 
-  subscriptions: Subscription[] = [];
-
   ngOnInit() {
-    this.subscriptions.push(this.store.select('category').subscribe(
-      ({list}) => this.collection = new CategoryCollection(list)
-    ));
+    this.collection = this.store.select('category').pipe(
+      map(({list}) => new CategoryCollection(list))
+    );
   }
-
-  ngOnDestroy() { this.subscriptions.forEach(subscription => subscription.unsubscribe()); }
 
   saveCategory() {
     if (this.category.getName()) {
