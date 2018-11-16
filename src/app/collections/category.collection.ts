ffb138fa7538e65703607ece1bb851ab649665ba
Olavi Sau <olavisau@gmail.com>
2018-11-16 13:50:25 +0200

    Dont overwrite constructor at all

--- src/app/collections/category.collection.ts
+++ src/app/collections/category.collection.ts
@@ -5,13 +5,10 @@ import {GenericCollection} from './generic.collection';
 
 export class CategoryCollection extends GenericCollection<CategoryModel> implements Iterable<CategoryModel> {
 
-  constructor(list: CategoryModel[]) {
-    super(list);
-  }
-
   completionPercent() {
     return infinityToZero(
       100 * sum(...this.list.map(c => c.completedPoints())) / sum(...this.list.map(c => c.totalPoints()))
     );
   }
+
 }
