ffab47ae233b5877dccd1260ebe306c76fedff2d
Olavi Sau <olavisau@gmail.com>
2018-11-19 22:41:03 +0200

    Create subscriber free solution

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -3,37 +3,29 @@ import {ActivatedRoute} from '@angular/router';
 import {AppState} from '../../app.state';
 import {Store} from '@ngrx/store';
 import {CategoryModel} from '../../models/category.model';
-import {AchievementModel} from '../../models/achievement.model';
 import {CategoryService} from '../../services/category.service';
 import {SetCategoryAction} from '../../actions/set-category.action';
-import {Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
+import {map, switchMap, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'ad-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
-export class CategoryComponent implements OnInit, OnDestroy {
+export class CategoryComponent implements OnInit {
 
-  category: CategoryModel;
-  routeSubscription: Subscription;
+  category$: Observable<CategoryModel>;
 
   constructor(private store: Store<AppState>, private route: ActivatedRoute, private categoryService: CategoryService) {}
 
   ngOnInit() {
-    this.routeSubscription = this.route.paramMap.subscribe((paramMap) => {
-          const key = paramMap.get('key')
-          this.store.select('category').subscribe(
-            categoryStore => this.category = categoryStore.list.find(category => category.getKey() === key)
-          );
-          this.categoryService.get(key).then(
-            category => this.store.dispatch(new SetCategoryAction(category))
-          );
-      }
+    this.category$ = this.route.paramMap.pipe(
+      map(paramMap => paramMap.get('key')),
+      tap(key => this.categoryService.get(key).then(category => this.store.dispatch(new SetCategoryAction(category)))),
+      switchMap(key => this.store.select('category').pipe(
+        map(categoryStore => categoryStore.list.find(c => c.getKey() === key))
+      ))
     );
-  }
-
-  ngOnDestroy() {
-    this.routeSubscription.unsubscribe();
   }
 }
